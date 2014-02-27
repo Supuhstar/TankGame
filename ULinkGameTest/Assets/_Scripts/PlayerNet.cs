1b9d47659850bca1ef221daa5132e80fd0cd6063
Supuhstar <supuhstar0@gmail.com>
2014-02-27 11:13:16 -0500

    Changed names, set up server-client communication structure
    Changed names, set up server-client communication structure. Going to
    ask Brandon to take care of filling the meat of the methods

---
+++ ULinkGameTest/Assets/_Scripts/PlayerNet.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Manages the player's network activities
+ */
+public class PlayerNet : MonoBehaviour {
+	
+	public string messtest = "NOTHING";
+
+	public enum Type
+	{
+		/** Represents a slow-shot cannon */
+		CANNON,
+		/** Represents a rapid-shot gun */
+		RAPID
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	[RPC]
+	void Move(string howToMove) {
+		Debug.Log ("I've been told to move: " + howToMove);
+	}
+
+	/** Signifies how long to wait before sending a message */
+	public float delay = .25f;
+	float expiry = 0;
+	public bool firedBefore = false, firedNow = false;
+	public Type currentType = Type.CANNON;
+	// Update is called once per frame
+	void Update () {
+		expiry += Time.deltaTime;
+		if (expiry >= delay) // ensure messages aren't sent too often
+		{
+			expiry = 0;
+			float fire1 = Input.GetAxis("Fire1");
+			firedNow = fire1 != 0; // if there is input from the fire control, the weapon has been fired. Else, it hasn't
+			if (currentType == Type.CANNON) // if the weapon fires like a cannon (slowly, one at a time)
+			{
+				if (firedBefore && firedNow) // if the weapon fired last update (control is still activated)
+				{
+					firedNow = false; // don't fire this time
+					firedBefore = true;
+				}
+				else
+					firedBefore = firedNow;
+			}
+			messtest = // build the message to send
+				"left " + Input.GetAxis("LeftTread") +
+				"; right " + Input.GetAxis("RightTread") +
+				"; fire " + (firedNow ? 1 : 0);
+
+			sendMessage(messtest);
+		}	
+	}
+
+	void sendMessage(string message)
+	{
+		uLink.NetworkView.Get(this).RPC("PlayerInput", uLink.RPCMode.Server, message);
+	}
+}
---
+++ ULinkGameTest/Assets/_Scripts/ServerNet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+/**
+ * Manages the server's network activities
+ */
+public class ServerNet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	[RPC]
+	void PlayerInput(string inputMessage)
+	{
+    	Debug.Log(inputMessage);
+		uLink.NetworkView.Get(this).RPC("Move", uLink.RPCMode.Owner, "You must move like: " + inputMessage);
+	} 
+}
