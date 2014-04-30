79ac218dd9969b9ef187abadefc4112e2987865a
Supuhstar <supuhstar0@gmail.com>
2014-04-30 15:33:21 -0400

    Finished new GUI!

---
+++ ULinkGameTest/Assets/_Scripts/HealthDisplayManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthDisplayManager : MonoBehaviour {
+
+	public Color lowHealthColor = Color.red;
+	public Color highHealthColor = Color.white;
+
+	public dfProgressBar progressBar;
+	public GameObject colorableModel;
+
+	public float health = 1.0f;
+	/*public float Health
+	{
+		get
+		{
+			return this.health;
+		}
+		set
+		{
+			this.health = value;
+			if (progressBar)
+				progressBar.Value = value;
+			if (colorableModel)
+				colorableModel.renderer.material.color =
+					Color.Lerp(lowHealthColor, highHealthColor, value);
+		}
+	}*/
+
+	// Use this for initialization
+	void Start () {
+		//Health = 1.0f;
+		SetHealth (health);
+	}
+
+	void OnGUI()
+	{
+		SetHealth (health);
+	}
+
+	public void SetHealth(float newHealth)
+	{
+		this.health = newHealth;
+		if (progressBar)
+			progressBar.Value = newHealth;
+		if (colorableModel)
+			colorableModel.renderer.material.color =
+				Color.Lerp(lowHealthColor, highHealthColor, newHealth);
+	}
+}
---
+++ ULinkGameTest/Assets/_Scripts/ResizeGUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeGUI : MonoBehaviour {
+
+	public dfGUIManager inGameUI;
+
+	// Use this for initialization
+	void Start () {
+		inGameUI = gameObject.GetComponent<dfGUIManager>();
+		inGameUI.FixedWidth = 1;
+		inGameUI.FixedHeight = 1;
+	}
+
+	public float delay = 2;
+	private float timeSinceLastUpdate = 0;
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnGUI() {
+		timeSinceLastUpdate += Time.deltaTime;
+		if (timeSinceLastUpdate > delay)
+		{
+			timeSinceLastUpdate = 0;
+			adjustGUISize ();
+		}
+	}
+
+	void adjustGUISize()
+	{
+		inGameUI.FixedWidth = Screen.width;
+		inGameUI.FixedHeight = Screen.height;
+	}
+}
